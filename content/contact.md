5b8e06b4e4a65ed5c40d8817d4061a72e01b325a
NorthOC <rev.denisas@gmail.com>
2023-06-08 16:22:24 +0300

    beauty

--- content/contact.md
+++ content/contact.md
@@ -1,9 +1,13 @@
 # Contact me
 
+<article id="contact-article">
+
 No contact form, just my email: 
 
 rev.denisas@gmail.com
 
 <button id="clipboard" onclick="copyToClipboard('clipboard')">Copy to clipboard</button>
+
+</article>
 
 <script src="/scripts/copyToClipboard.js"></script>
--- scripts/copyToClipboard.js
+++ scripts/copyToClipboard.js
@@ -2,5 +2,6 @@ function copyToClipboard(element_id) {
     navigator.clipboard.writeText("rev.denisas@gmail.com");
     let btn = document.getElementById(element_id);
     btn.innerHTML = "Copied!";
-    btn.style.backgroundColor = "var(--green1)"
+    btn.style.boxShadow = "0px 0px 1px 1px var(--fa-pip2)"
+    btn.style.color = "var(--fa-pip2)"
 };
